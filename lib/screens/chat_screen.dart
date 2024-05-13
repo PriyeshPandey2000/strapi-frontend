f24c6f019324797f675db746786a9f6898838b3f
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-13 15:24:38 +0530

    .

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,7 +20,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void initState() {
     super.initState();
     // Initialize WebSocketService with WebSocketChannel
-    final channel = WebSocketChannel.connect(Uri.parse('wss://strapi-backend-6y46.onrender.com:1440'));
+    final channel = WebSocketChannel.connect(Uri.parse('wss://strapi-backend-6y46.onrender.com'));
     webSocketService = WebSocketService(channel);
 
     // Receive messages and update UI
